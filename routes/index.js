cf18d72444b4d7d8ac059d3a5a36e7f743ed49a4
chentao <3413218@gmail.com>
2019-04-14 17:46:06 +0800

    完成数据对接

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
+var config = require('../src/config');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'reply' });
+  let houseList = [], error = null;
+  try{
+    fs.statSync(config.dataDir);
+    houseList = JSON.parse(fs.readFileSync(config.dataDir,'utf-8'))
+  }catch(e) {
+    //捕获异常
+    error = e.ENOENT;
+  }
+  res.render('index', { title: '房源', houses:houseList, error:error });
 });
 
 module.exports = router;
--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
 var path = require('path');
-
-module.exports.dataDir = path.resolve(__dirname, '../test/record.json');
+module.exports.dataDir = path.resolve('/root/rentHouse/record.json');
